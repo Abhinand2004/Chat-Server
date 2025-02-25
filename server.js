f59a676b4d66961d5ecaff56129dd7b67dfbf92a
abhiand <abhinandc293@gmail.com>
2025-02-25 10:26:17 +0530

    new-urls

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://msg-mate-eight.vercel.app"],
     methods: ["GET", "POST"],
     credentials: true,
   }
 });
 
-app.use(cors({  origin: ["http://localhost:5173"], credentials: true }));
+app.use(cors({  origin: ["http://localhost:5173","https://msg-mate-eight.vercel.app"], credentials: true }));
 app.use(express.json({ limit: "50mb" }));
 app.use(passport.initialize()); 
 
